9bb216f26653f2f725ffe8acc1e7e343fa03ddfa
Gaston <mariomazza01@gmail.com>
2018-11-03 17:41:45 -0400

    h

--- client/src/components/Auth/ErrorMessage.jsx
+++ client/src/components/Auth/ErrorMessage.jsx
@@ -3,16 +3,21 @@ import { connect } from "react-redux";
 
 
 const ErrorMessage = ({ error }) => {
-  let ans= ''
-  if(typeof error.message.message !== 'undefined'){
-    if(error.message.message === 'Object(...) is not a function' ){
-      ans=''
-    }else if(error.message.message.length >= 38){
-      ans= error.message.message.substring(38, 61);
-    }else{
-      ans=error.message.message
+  let ans= '';
+  try {
+    if(typeof error.message.message !== 'undefined'){
+      if(error.message.message === 'Object(...) is not a function' ){
+        ans=''
+      }else if(error.message.message.length >= 38){
+        ans= error.message.message.substring(38, 61);
+      }else{
+        ans=error.message.message
+      }
     }
+  } catch (error) {
+    ans='';
   }
+  
 
   return(
   <Fragment>
