1f794fc57a7b10ac77d3111460385e674bab8b95
Gaston <mariomazza01@gmail.com>
2018-11-04 21:00:00 -0500

    changed redux store dev

--- client/src/redux/store/configureStore.js
+++ client/src/redux/store/configureStore.js
@@ -1,74 +1,46 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/rootReducer";
 import thunk from 'redux-thunk'
-
-
-//  const configureStoreDev = preloadedState => {
-//   const middlewares = [thunk];
-//   const middleWareEnhancers = applyMiddleware(...middlewares);
-
-//   const storeEnhancers = [middleWareEnhancers];
-
-//   // const composeEnhancers = compose(...storeEnhancers);
-
-//   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose(...storeEnhancers); // add support for Redux dev tools
-
-//   const store = createStore(rootReducer, preloadedState, composeEnhancers( applyMiddleware(...middlewares) ) );
-
-
-
-//   // const store = createStore(rootReducer, preloadedState, composeEnhancers);
-
-//   if (module.hot) {
-//     // Enable Webpack hot module replacement for reducers
-//     module.hot.accept('../reducers/rootReducer', () => {
-//       const nextReducer = require('../reducers/rootReducer').default; // eslint-disable-line global-require
-//       store.replaceReducer(nextReducer);
-//     });
-//   }
-
-//   return store;
-// };
-
-
-// const configureStoreProd=(initialState)=> {
-//   // const reactRouterMiddleware = routerMiddleware(history);
-//   const middlewares = [
-//     // Add other middleware on this line...
-
-//     // thunk middleware can also accept an extra argument to be passed to each thunk action
-//     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-//     thunk,
-//     // reactRouterMiddleware,
-//   ];
-
-//   return createStore(rootReducer, initialState, compose(
-//     applyMiddleware(...middlewares)
-//     )
-//   );
-// }
-
-// const configureStore = process.env.NODE_ENV === 'production' ? configureStoreProd : configureStoreDev;
-
-
-// export default configureStore;
-
+// import { composeWithDevTools } from 'redux-devtools-extension'
 
 
 const DEFAULT_STATE = {
-  auth: { isAuthenticated: false },
-  // error: { message: null },
-  
-};
+  auth: { isAuthenticated: false }
+}; 
 
- const configureStore = createStore(
-  rootReducer,
-  DEFAULT_STATE,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+
+
+//  const configureStore = (preloadedState)=>{
+//     const middlewares = [thunk];
+//     const middlewareEnhancer = applyMiddleware(...middlewares);
+
+//     const storeEnhancers = [middlewareEnhancer];
+
+//     const composeEnhancer = composeWithDevTools(...storeEnhancers);
+
+
+//     const store = createStore(
+
+//         rootReducer,
+//         preloadedState,
+//         composeEnhancer
+//     );
+
+//     if(process.env.NODE_ENV !== 'production'){
+//         if(module.hot){
+//             module.hot.accept('../reducers/rootReducer.js', ()=>{
+//                 const newRootReducer = require('../reducers/rootReducer').default;
+//                 store.replaceReducer(newRootReducer)
+//             })
+//         }
+//     }
+
+//     return store;
+// }
+
+
+ const configureStore = createStore( rootReducer, DEFAULT_STATE,
+  compose( applyMiddleware(thunk) ),
 );
 
 export default configureStore
